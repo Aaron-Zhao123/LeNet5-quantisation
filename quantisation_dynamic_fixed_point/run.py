b4f99d7fd1f130a6475291767bcdf758b22a220f
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-06 01:25:37 +0100

    try vary dynamic range

--- quantisation_dynamic_fixed_point/run.py
+++ quantisation_dynamic_fixed_point/run.py
@@ -10,14 +10,14 @@ base_name = 'base'
 learning_rate = 1e-5
 quantisation_bits = [2,4,8,16,32,64]
 # dynamic_width = [2,4,8,16,32,64]
-dynamic_width = [8,16,32,64]
+dynamic_width = [1,2,3,4,5]
 # quantisation_bits = [32]
 pre_train_acc_list = []
 test_acc_list = []
 for d_width in dynamic_width:
     # set Parameters
     param = [
-    ('-quantisation_bits', 4),
+    ('-quantisation_bits', 2),
     ('-parent_dir', './'),
     ('-base_name', base_name),
     ('-d_range', d_width)
