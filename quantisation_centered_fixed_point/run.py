dfcbcbabcad9252d4d7fc15bf87db233ec70ee6c
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-09 18:36:40 +0100

    tmp

--- quantisation_centered_fixed_point/run.py
+++ quantisation_centered_fixed_point/run.py
@@ -2,6 +2,10 @@ import os
 import cdfp_training
 import pickle
 import numpy as np
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+import pylab
 
 acc_list = []
 count = 0
@@ -15,8 +19,11 @@ quantisation_bits = [2,4,8,16,32,64]
 # quantisation_bits = [32]
 pre_train_acc_list = []
 test_acc_list = []
+PLOT = 1
 with open('weights/base.pkl', 'rb') as f:
     wc1, wc2, wd1, out, bc1, bc2, bd1, bout = pickle.load(f)
+with open('masks/base.pkl', 'rb') as f:
+    weights_mask = pickle.load(f)
 weights_val = {
     'cov1': wc1,
     'cov2': wc2,
@@ -24,16 +31,34 @@ weights_val = {
     'fc2': out
 }
 
+
+if (PLOT):
+    df = (weights_val['fc1'] * weights_mask['fc1']).flatten()
+    df = df[df!=0]
+    n, bins, patches = plt.hist(df, 100, normed=1, facecolor='blue', alpha=0.75)
+    plt.grid(True)
+    plt.xlabel('Smarts')
+    plt.ylabel('Probability')
+    plt.savefig("test.png")
+
+
+
 keys = ['cov1', 'cov2', 'fc1', 'fc2']
 central_value = {}
 c_pos = {}
 c_neg = {}
 
 for key in keys:
-    central_value[key] = np.mean(weights_val[key])
-    c_pos[key] = np.mean(weights_val[key][weights_val[key] > central_value[key]])
-    c_neg[key] = np.mean(weights_val[key][weights_val[key] <= central_value[key]])
+    central_value[key] = np.mean(weights_val[key]* weights_mask[key])
+    pos_plus = np.logical_and((weights_val[key]* weights_mask[key]) > central_value[key], weights_mask[key])
+    pos_minus = np.logical_and((weights_val[key]* weights_mask[key]) <= central_value[key], weights_mask[key])
+    c_pos[key] = np.mean(weights_val[key][pos_plus])
+    c_neg[key] = np.mean(weights_val[key][pos_minus])
 
+print(central_value)
+print(c_pos)
+print(c_neg)
+# sys.exit()
 
 
 for q_width in quantisation_bits:
