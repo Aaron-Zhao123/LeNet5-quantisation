32bd705abe6e95854d21456b0a5a44a4019535b1
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-15 23:36:04 +0100

    retrain

--- quantisation_fixed_point/quantisation_training.py
+++ quantisation_fixed_point/quantisation_training.py
@@ -311,7 +311,7 @@ def main(argv = None):
                         elif (q_bits == 4):
                             threshold = 0.99
                         elif (q_bits == 8):
-                            threshold = 0.993
+                            threshold = 0.9936
                         elif (q_bits == 16):
                             threshold = 0.9936
                         elif (q_bits == 32):
