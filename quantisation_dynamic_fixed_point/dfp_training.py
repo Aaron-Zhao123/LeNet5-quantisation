fbd8acfab5dab3cd9ceeb90596170df5ee3cbb68
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-16 13:43:40 +0100

    retrain

--- quantisation_dynamic_fixed_point/dfp_training.py
+++ quantisation_dynamic_fixed_point/dfp_training.py
@@ -302,10 +302,22 @@ def main(argv = None):
                     accuracy_mean = np.mean(accuracy_list)
                     if (i % 1000 == 0):
                         print('cost and acc:',c,accuracy_mean)
-                    if (accuracy_mean > 0.99):
+                    if (accuracy_mean > 0.9):
+                        accuracy_list = np.zeros(20)
                         test_acc = accuracy.eval({  x:mnist.test.images,
                                                     y: mnist.test.labels})
                         print('Try quantize {} frac bits, test accuracy is {}'.format(q_bits, test_acc))
+                        if (q_bits == 2):
+                            threshold = 0.99
+                        elif (q_bits == 4):
+                            threshold = 0.9936
+                        elif (q_bits == 8):
+                            threshold = 0.9936
+                        elif (q_bits == 16):
+                            threshold = 0.9936
+                        elif (q_bits == 32):
+                            threshold = 0.9936
+
                         if (test_acc > 0.9936):
                             print('Training ends because accuracy is high')
                             with open(parent_dir+'weights/'+ 'quanfp' + str(q_bits) +'.pkl','wb') as f:
